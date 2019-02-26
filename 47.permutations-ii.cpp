8e12c3bed2c178fcec679c519c872805b71e91f9
lightlov <33197167+lightlov@users.noreply.github.com>
2019-02-26 11:07:10 +0800

    update

--- 47.permutations-ii.cpp
+++ 47.permutations-ii.cpp
@@ -27,77 +27,9 @@
  * 
  * 
  */
-static const auto __ = []() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	return nullptr;
-}();
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> combination;
-        vector<vector<int>> res;
-        sort(candidates.begin(),candidates.end());
-//        for(int i=0;i<candidates.size();i++){
-//            if(i>0&&candidates[i]==candidates[i-1]){continue;}
-        func(res,combination,candidates,target,0);
-//        }
-        return res;
-    }
-    
-    void func(vector<vector<int>> &res,vector<int> &combination,vector<int>& candidates, int target,int cnt){
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
         
-        if(cnt>=candidates.size()) return;
-//        combination.push_back(candidates[cnt]);
-//        target-=candidates[cnt];
-//        if(target<0){return;}
-//        else if(target==0){res.push_back(combination);return;}
-        for(int i=cnt;i<candidates.size();i++){
-            if(target<candidates[i]) break;
-            if(i>cnt&&candidates[i]==candidates[i-1]){continue;}
-            combination.push_back(candidates[i]);
-            if(target>candidates[i]){
-                target-=candidates[i];
-                func(res,combination,candidates,target,i+1);
-                target+=candidates[i];
-            }
-            else{
-                res.push_back(combination);
-            }
-            combination.pop_back();
-            
-            
-//            func(res,combination,candidates,target,i);
-            
-        }
     }
 };
-/*
-class Solution {
-public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> combination;
-        vector<vector<int>> res;
-        sort(candidates.begin(),candidates.end());
-        for(int i=0;i<candidates.size();i++){
-            if(i>0&&candidates[i]==candidates[i-1]){continue;}
-            func(res,combination,candidates,target,i);
-        }
-        return res;
-    }
-    
-    void func(vector<vector<int>> &res,vector<int> combination,vector<int>& candidates, int target,int cnt){
-        
-        
-        combination.push_back(candidates[cnt]);
-        target-=candidates[cnt];
-        if(target<0){return;}
-        else if(target==0){res.push_back(combination);return;}
-        for(int i=cnt+1;i<candidates.size();i++){
-            if(i>cnt+1&&candidates[i]==candidates[i-1]){continue;}
-            func(res,combination,candidates,target,i);
-            
-        }
-    }
-};
-*/
